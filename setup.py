628f676a4f5e3068b30d27fb09e2a1ae67af3aee
Bobby Dilley <bobby@dilley.uk>
2020-10-29 22:14:06 +0000

    Add executable

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='rominfo',  
+	version='0.1',
+	scripts=['rominfo'] ,
+	author="Bobby Dilley",
+	author_email="bobby@dilley.uk",
+	description="A command line utility to read SEGA arcade rom info",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/bobbydilley/rominfo",
+	packages=setuptools.find_packages(),
+)
